79d04a64e638719774151bd4ba4755c50ebce657
dhanidbr <dhanidbr.private@gmail.com>
2020-03-25 23:22:19 +0700

    uts2

---
+++ app/src/main/java/com/example/profilfakultasupn/ProdiData.kt
@@ -0,0 +1,4 @@
+package com.example.profilfakultasupn
+
+data class ProdiData (val namaFakultas: String, val deskripsiFakultas: String, val bulletFakultas: String, val image:Int, val email: String, val web: String
+    )
